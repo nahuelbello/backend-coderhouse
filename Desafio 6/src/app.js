28f213b065439d535d46390239a4e86c92c3017f
Nahuel Bello <nahuel.bello@gmail.com>
2023-02-14 23:01:50 -0300

    Termino rutas y vistas

--- Desafio 6/src/app.js
+++ Desafio 6/src/app.js
@@ -1,29 +1,41 @@
+// Libraries - Frameworks
 import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import { Server } from "socket.io"
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cookieParser from "cookie-parser";
 import handlebars from "express-handlebars";
+
+// Routers
 import chatRouter from "./routes/chat.router.js";
 import viewsRouter from "./routes/views.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import loginRouter from "./routes/login.router.js";
+import registerRouter from "./routes/register.router.js";
 import productsRouter from "./routes/products.router.js";
+
+// DAO
 import { ProductManager } from "./dao/dbManager.js";
 import messagesModel from "./dao/models/messages.model.js";
-
-
-dotenv.config();
-
-
 const productManager = new ProductManager();
 
 
+// ENV
+dotenv.config();
+const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9jlfml1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+// Server
 const app = express();
 const PORT = process.env.PORT || 8080;
 const httpServer = app.listen(PORT, () => console.log(`Servidor escuchando en http://localhost:${PORT}`));
 const io = new Server(httpServer);
 
 
+// Handlebars
 app.set("views", "./src/views/");
 app.set("view engine", "hbs");
 app.engine("hbs", handlebars.engine({
@@ -34,20 +46,36 @@ app.engine("hbs", handlebars.engine({
 }));
 
 
+// Middlewares
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: DB_URL,
+        mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+        ttl: 10000
+    }),
+    secret: 'codigo-s3cr3t0',
+    resave: true,
+    saveUninitialized: true
+}));
 
 
-app.use("/api/products", productsRouter);
-app.use("/api/carts", cartsRouter);
+// Routes
 app.use("/", viewsRouter);
 app.use("/chat", chatRouter);
+app.use('/api/login', loginRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/api/products", productsRouter);
+app.use('/api/register', registerRouter);
 
 
+// Database
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9jlfml1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, (error) => {
-    if (error) {
+mongoose.connect(DB_URL, (err) => {
+    if (err) {
         console.log("Error de conexion");
     } else {
         console.log("Conectado a la base de datos");
@@ -55,6 +83,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cl
 });
 
 
+// Sockets
 io.on("connection", socket => {
     console.log("Cliente conectado");
 
--- Desafio 6/src/routes/register.router.js
+++ Desafio 6/src/routes/register.router.js
@@ -5,15 +5,15 @@ import registerModel from "../dao/models/register.model.js";
 const registerRouter = Router();
 
 
-registerRouter.get('/', (req, res) => {
-    res.render('register', {});
+registerRouter.get("/", (req, res) => {
+    res.render("register", {});
 });
 
-registerRouter.post('/', async (req, res) => {
+registerRouter.post("/", async (req, res) => {
     try {
         const { first_name, last_name, email, age, password } = req.body;
-        const response = await registerModel.create({ first_name, last_name, email, age, password });
-        res.status(200).send({ message: 'success', payload: response });
+        const payload = await registerModel.create({ first_name, last_name, email, age, password });
+        res.status(200).send({ message: "success", payload: payload });
     } catch (err) {
         res.status(500).send(err.message);
     }
